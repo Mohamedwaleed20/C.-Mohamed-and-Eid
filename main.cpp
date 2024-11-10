3e0c500b99a35e1173e0d68edfd753bb9fb16ef5
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-11-10 18:38:01 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main()
+{
+   while(true)
+   {
+       string a;
+       cin >> a ;
+       if(a=="Mohamed")
+       {
+           cout<<"Mohamed and Eid are brothers Mohamed died who is left?"<<endl;
+           cout << "NO" << endl ;
+           break;
+       }
+       if(a=="Eid")
+       {
+            cout<<"Mohamed and Eid are brothers Mohamed died who is left?"<<endl;
+       }
+   }
+
+
+
+    return 0;
+}
